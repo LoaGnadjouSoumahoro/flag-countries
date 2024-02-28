6911cc52f206ca7fa459b8c3ae0480156f70e120
Loa Gnadjou Soumahoro <loagnadjou@gmail.com>
2024-02-28 13:04:01 +0100

    Creation des pages et de la navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
 
-function App() {
-  return <></>;
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* le path="*" fonction si l'urlne correspond à rien */}
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
