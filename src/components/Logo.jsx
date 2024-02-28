ebf44b24809e27f45a38b5585790eed086645cf5
Loa Gnadjou Soumahoro <loagnadjou@gmail.com>
2024-02-28 16:26:18 +0100

    Recupération de données des cartes via API et leurs affichage

---
+++ src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div className="logo">
+      <img src="" alt="Logo" />
+      <h3>Web Premium</h3>
+    </div>
+  );
+};
+
+export default Logo;
